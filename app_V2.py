542ff0393b7113fa268534bc0162d3796fd92a95
Kendrake Tsui <kjtsui@yahoo.com>
2021-06-22 00:35:31 -0700

    feat: Add core features to client and asyncio server
    Update client.py so it no longer acts as a pipe that just sends user
    input to the asyncio server. client.py now cycles through client.csv on
    user input and sends a bytestream and lboxes array for each image. This
    update implements the core functionality we want from the client in that
    it sends over an image bytestream and metadata, similar to what scrubcam
    does.
    
    The asyncio server code was also updated in app_V2.py to read in the
    image bytestream and lboxes array messages. The asyncio server writes
    the image to disk and adds the entry to image_log.csv. It then packages
    the saved image filename and image class name in a dictionary and adds
    it to the shared queue that sends the information to the dash server.

--- app_V2.py
+++ app_V2.py
@@ -8,34 +8,85 @@ from dash.dependencies import Input, Output, State
 import asyncio
 import base64
 from multiprocessing import Process, Queue
+import struct
+import pickle
 
 """
-This program consists of 3 processes:
-    1. main
-    2. asyncio server
-    3. dash server
+This program consists of 3 processes: 1. main 2. asyncio server 3. dash server
 
-The main process creates a shared Queue and uses multiprocessing to spawn an asyncio processes and a dash processes. The shared Queue is passed as a parameter to each process. 
+The main process creates a shared Queue and uses multiprocessing to spawn an
+asyncio processes and a dash processes. The shared Queue is passed as a
+parameter to each process.
 
-The asyncio server listens on port 8888 on localhost and puts any messages it receives into the shared Queue. Every 2 seconds, the dash server will check the Queue to see if anything was added to it. If so, it gets each element and adds it to the website. The website ends up rendering all the text that was received by the asyncio server.
+The asyncio server listens on port 8888 on localhost and puts any messages it
+receives into the shared Queue. Every 2 seconds, the dash server will check the
+Queue to see if anything was added to it. If so, it gets each element and adds
+it to the website. The website ends up rendering all the text that was received
+by the asyncio server.
 
-app_V2.py is best used with client.py from 6-18-2021. One can also send messages to the asyncio terminal by executing `nc localhost 8888` in another terminal and typing in a message.
+app_V2.py is best used with client.py from 6-18-2021. One can also send
+messages to the asyncio terminal by executing `nc localhost 8888` in another
+terminal and typing in a message.
 """
 
 
 def start_asyncio(queue):
+    import os
+    import json
     import asyncio
-    
-    # reads in a message from a client and prints out the message received
+    from datetime import datetime
+
+    RECORD_FOLDER = 'saved_images/'
+
+    def save_image_to_disk(image, class_name):
+        now = datetime.now()
+        timestamp = now.strftime('%Y-%m-%dT%Hh%Mm%Ss.%f')[:-3]
+        filename = '{}_{}.jpeg'.format(timestamp, class_name)
+        filepath = os.path.join(RECORD_FOLDER, filename)
+
+        # saving image to disk
+        with open(filepath, 'wb') as saved_img:
+            saved_img.write(image)
+
+        # update csv log that stores all image records
+        with open('image_log.csv', 'a') as image_csv:
+            image_csv.write('{},{}\n'.format(filepath, class_name))
+
+        return filename
+
+    # reads in two messages from client and prints out the messages received
+
     async def handle_echo(reader, writer):
-        data = await reader.read(100)
-        message = data.decode()
-        print("{}".format(message))
+        # read size of image bytestream
+        image_struct = await reader.read(struct.calcsize('<L'))
+        image_size = struct.unpack('<L', image_struct)[0]
+        # for debugging: print(imae_size)
+
+        # read in image bytestream
+        image = await reader.readexactly(image_size)
+
+        # read size of lboxes struct
+        lboxes_struct = await reader.read(struct.calcsize('<L'))
+        lboxes_size = struct.unpack('<L', lboxes_struct)[0]
+
+        # read in lboxes bytestream
+        lboxes_struct = await reader.readexactly(lboxes_size)
+        lboxes = pickle.loads(lboxes_struct)
+        # for debugging: print(lboxes)  # ex: [{'class_name': 'cheetah'}]
+        class_name = lboxes[0]['class_name']
+
+        # save image to disk and to the csv
+        filename = save_image_to_disk(image, class_name)
+
+        # send image path and class name to dash server
+        message = {"img_path": filename, "label": class_name}
         queue.put(message)
 
-    # reference https://github.com/CS131-TA-team/UCLA_CS131_CodeHelp/blob/master/Python/echo_server.py
-    # creates a server that listens on localhost at port 8888 for incoming messages
-    # all incoming messages are handled by handle_echo(reader, writer)
+    # reference:
+    # https://github.com/CS131-TA-team/UCLA_CS131_CodeHelp/blob/master/Python/echo_server.py
+    # creates a server that listens on localhost at port 8888 for incoming
+    # messages all incoming messages are handled by handle_echo(reader, writer)
+
     async def run_forever():
         server = await asyncio.start_server(handle_echo, '127.0.0.1', 8888)
 
@@ -47,28 +98,32 @@ def start_asyncio(queue):
 
     asyncio.run(run_forever())
 
+
 def start_dash(queue):
     import dash
     import dash_core_components as dcc
     import dash_bootstrap_components as dbc
     import dash_html_components as html
     from dash.dependencies import Input, Output, State
-    
 
-    app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+    app = dash.Dash(
+        __name__,
+        external_stylesheets=[
+            dbc.themes.BOOTSTRAP],
+        suppress_callback_exceptions=True)
     app.layout = html.Div([
         html.Div("", id='content'),
         dcc.Interval(
             id='interval-component',
-            interval=1.5*1000, # in milliseconds
+            interval=1.5 * 1000,  # in milliseconds
             n_intervals=0
         )
     ])
-    
+
     # updates content displayed on page
     @app.callback(Output('content', 'children'),
-                 [Input('interval-component', 'n_intervals')], 
-                 [State('content', 'children')], prevent_initial_call = True)
+                  [Input('interval-component', 'n_intervals')],
+                  [State('content', 'children')], prevent_initial_call=True)
     def empty_queue(n_intervals, content):
         # prints everything in the Queue
         while not queue.empty():
@@ -77,6 +132,7 @@ def start_dash(queue):
         return(content)
 
     app.run_server()
+
 
 if __name__ == '__main__':
     q = Queue()
--- client.py
+++ client.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 
 import asyncio
+import pickle
+import struct
+import pandas as pd
+
 
 """
 referenced from:
 https://github.com/CS131-TA-team/UCLA_CS131_CodeHelp/blob/master/Python/echo_client.py
 
-To run with server_example.py:
-1. start server_example.py first in a terminal
-2. start client.py in another terminal
-3. follow print-back instructions on client side until you quit
-
-You should see messages print in the server_example.py terminal
-
 To run with app_V2.py:
 1. start app_V2.py first in a terminal
 2. start client.py in another terminal
-3. follow print-back instructions on client side until you quit
+3. press 'enter' to send a message to the dash server or 'quit' to quit
 
-You should see messages appear on the website page on localhost:8050
+You should see messages containing the image path and the class name appear on
+the website page on localhost:8050
+
+This client cycles through images in a csv and sends the image bystream, along
+with metadata, to the asyncio server.
 """
 
 
@@ -37,26 +38,77 @@ class Client:
         self.port = port
         self.name = name
         self.message_max_length = int(message_max_length)
+        """
+        member variables for sending image path from csv
+        """
+        self.df = pd.read_csv('client.csv')
+        self.parity = 0
+        self.animal = 0
+        self.row = 0
+        self.image_path = self.df.iloc[self.row].values[0]
+        self.image_bytes = None
+        self.class_name = self.df.iloc[self.row].values[1]
 
-    async def tcp_echo_client(self, message):
+    def update_img(self):
+        # updating parity if animal == 1 (iterated through all animals)
+        self.parity = 1 - self.parity if self.animal == 11 else self.parity
+        # updating animal image to retrieve from csv
+        self.animal = 0 if self.animal == 11 else self.animal + 1
+        # updating row to retrieve
+        self.row = (((self.animal) * 2) +
+                    1) % 24 if self.parity else ((self.animal) * 2) % 24
+        self.image_path = self.df.iloc[self.row].values[0]
+        self.class_name = self.df.iloc[self.row].values[1]
+
+    async def tcp_echo_client(self):
         """
-        on client side send the message for echo
+        on client side send the image for echo
         """
+
+        # opening socket
         reader, writer = await asyncio.open_connection(self.ip, self.port)
-        print(f'{self.name} send: {message!r}')
-        writer.write(message.encode())
 
-        print('close the socket')
+        # obtaining the image bytes
+        with open(self.image_path, 'rb') as image_file:
+            self.image_bytes = image_file.read()
+
+        # send_image
+        # for debugging: print(len(self.image_bytes))
+        writer.write(struct.pack('<L', len(self.image_bytes)))
+        await writer.drain()
+
+        # sending image bytes first
+        writer.write(self.image_bytes)
+        await writer.drain()
+
+        # lboxes is array of dictionaries
+        lboxes = [{"class_name": self.class_name}]
+        lboxes_bytes = pickle.dumps(lboxes)
+
+        # send lboxes second
+        # for debugging: print("lboxes len: ", len(lboxes_bytes))
+        writer.write(struct.pack('<L', len(lboxes_bytes)))
+        await writer.drain()
+        writer.write(lboxes_bytes)
+        await writer.drain()
+
+        print('closing the socket')
         # The following lines closes the stream properly If there is any
         # warning, it's due to a bug o Python 3.8:
         # https://bugs.python.org/issue38529 Please ignore it
         writer.close()
+        await writer.wait_closed()
+
+        # # updating next image to send
+        print('updating image')
+        self.update_img()
 
     def run_until_quit(self):
         # start the loop
         while True:
             # collect the message to send
-            message = input("Please input the next message to send: ")
+            message = input(
+                "Enter any key to send a new image or enter 'quit' to exit: ")
             if message in [
                 'quit',
                 'exit',
@@ -67,7 +119,7 @@ class Client:
                     '(exit)']:
                 break
             else:
-                asyncio.run(self.tcp_echo_client(message))
+                asyncio.run(self.tcp_echo_client())
 
 
 if __name__ == '__main__':
